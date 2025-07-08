c353cd6c2659f1c6530a4f8ed01c68717fe06982
Pommy <tm_git@93m.org>
2025-07-08 16:51:57 +0200

    app.93million.nextcloud: enabled sharing access to rapptor files

--- app.93million.nextcloud/scripts/configure.sh
+++ app.93million.nextcloud/scripts/configure.sh
@@ -4,8 +4,5 @@ set -e
 
 if ! php /var/www/html/occ files_external:list | grep "/Rapptor files" > /dev/null; then
   php /var/www/html/occ app:enable files_external
-  php /var/www/html/occ \
-    files_external:create \
-    -c datadir="/files/" \
-    -- "/Rapptor files" local "null::null"
+  php /var/www/html/occ files_external:import /scripts/files_external_volumes.json
 fi

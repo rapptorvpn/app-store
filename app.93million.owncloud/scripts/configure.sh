5b553c2dd7b25056d79de01537c186343bc47935
Pommy <tm_git@93m.org>
2025-07-14 20:58:31 +0200

    app.93million.owncloud: enabled write access to rapptor files

--- app.93million.owncloud/scripts/configure.sh
+++ app.93million.owncloud/scripts/configure.sh
@@ -4,8 +4,6 @@ set -e
 
 if ! occ files_external:list | grep "/Rapptor files" > /dev/null; then
   occ app:enable files_external
-  occ files_external:create \
-    -c datadir="/files/" \
-    -- "/Rapptor files" local "null::null"
+  occ files_external:import /scripts/files_external_volumes.json
   occ config:app:set --value yes core enable_external_storage
 fi

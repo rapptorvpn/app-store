f44ace38bc4ae8007ee8916f21fcf716d46df9ab
Pommy <tm_git@93m.org>
2026-03-19 12:56:15 +0100

    app.93million.nextcloud: build in external repo

--- app.93million.nextcloud/scripts/configure.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/env bash
-
-set -e
-
-if ! php /var/www/html/occ files_external:list | grep "/Rapptor files" > /dev/null; then
-  php /var/www/html/occ app:enable files_external
-  php /var/www/html/occ files_external:import /scripts/files_external_volumes.json
-fi
